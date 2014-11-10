d5f044626bf5f382b3a700d7770df0ec2b942f69
gonzalo <gestayv@hotmail.es>
2014-11-09 22:10:22 -0300

    cambios movimiento

--- src/Modelo/PrimeraFase.java
+++ src/Modelo/PrimeraFase.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Modelo;
 
-/**
- *
- * @author Casa
- */
+
 public class PrimeraFase {
+    
+    boolean medallaUno, medallaDos, medallaTres, medallaCuatro, 
+            medallaCinco, medallaSeis, medallaSiete, medallaOcho;
     
 }
